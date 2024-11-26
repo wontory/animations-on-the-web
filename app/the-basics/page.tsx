8411fb83bac9ab05a74abeff9093b80cec2684fe
조성원 <tjddnjsjo@kakao.com>
2024-11-26 21:34:31 +0900

    refactor: update layout

--- app/the-basics/page.tsx
+++ app/the-basics/page.tsx
@@ -12,7 +12,7 @@ import { RefactoredSmoothButton } from "~/components/the-basics/variants";
 
 export default function TheBasics() {
   return (
-    <div className="grid min-h-dvh max-w-dvw items-center justify-items-center gap-20 bg-black text-center">
+    <div className="grid min-h-dvh max-w-dvw items-center justify-items-center gap-20 bg-black py-20 text-center">
       <h1 className="text-4xl text-white">The Basics</h1>
       <section className="flex flex-col items-center gap-4">
         <h2 className="text-2xl text-white">Anatomy of an animation</h2>
