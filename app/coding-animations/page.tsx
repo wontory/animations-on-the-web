80f956af5ed202a9ba34f6f449ccce61199315ef
조성원 <tjddnjsjo@kakao.com>
2024-11-28 01:05:23 +0900

    feat: create main pages

--- app/coding-animations/page.tsx
+++ app/coding-animations/page.tsx
@@ -1,13 +1,10 @@
 import Link from "next/link";
 
-export default function HomePage() {
+export default function CodingAnimationsPage() {
   return (
     <>
-      <h1 className="text-4xl text-white">Animations on the web</h1>
+      <h1 className="text-4xl text-white">Coding animations</h1>
       <section className="flex flex-col items-center gap-4">
-        <h2 className="text-2xl text-white">
-          <Link href="/coding-animations">Coding animations</Link>
-        </h2>
         <ul className="flex flex-col gap-2 text-white">
           <li>
             <Link href="/coding-animations/the-basics">The Basics</Link>
