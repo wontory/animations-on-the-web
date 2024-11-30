547491615bd72aeeca53b6c6e157f9b333585e36
조성원 <tjddnjsjo@kakao.com>
2024-11-30 21:15:38 +0900

    feat: create "Step animation" page

---
+++ app/coding-animations/multi-step-component/page.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+import { MultiStepComponent } from "~/components/multi-step-component/step-animation";
+
+export const metadata: Metadata = {
+  title: "Multi-step component",
+  description: "",
+};
+
+export default function FeedbackPopoverPage() {
+  return (
+    <>
+      <h1 className="text-4xl text-white">Multi-step component</h1>
+      <section className="flex flex-col items-center gap-4">
+        <h2 className="text-2xl text-white">Step animation</h2>
+        <MultiStepComponent />
+      </section>
+    </>
+  );
+}
