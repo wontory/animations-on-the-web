8261b3f4cb29536dc38f8a5419f29812da9921d6
조성원 <tjddnjsjo@kakao.com>
2024-12-02 03:12:55 +0900

    refactor: update layout.tsx to use cn utility function

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 
 import "~/styles/globals.css";
+import { cn } from "~/utils/cn";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(geistSans.variable, geistMono.variable, "antialiased")}
       >
         <div className="flex min-h-dvh max-w-dvw flex-col items-center justify-center gap-20 bg-black py-20 text-center">
           {children}
